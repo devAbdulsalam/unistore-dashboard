6a0dd97fcc38cac0910b4308f5c7325591796c46
devabdulsalam <ammuftau74@gmail.com>
2024-05-31 12:51:45 +0100

    add product category

--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -208,7 +208,7 @@ const AddProduct = () => {
                       className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                     >
                       <option value="">Category</option>
-                      {categoriesData.map((item, index) => {
+                      {categoriesData?.map((item, index) => {
                         return (
                           <option key={index} value={item.id}>
                             {item.name}
